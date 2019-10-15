d75abedb1859fe54390ef8e524cce7beefa6d5b2
qazxswqw2 <qazxswqw2@gmail.com>
2019-10-15 15:44:35 +1100

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <div class="header-left">Back
+        <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        Content
+    </div>
+    <div class="header-right">
+        City
+        <span class="iconfont">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+</style>
